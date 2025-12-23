ba8a46be891855dfd7cd8b942f13c0cf56b535a3
Milind Singh <milind345@gmail.com>
2025-12-22 22:51:34 -0800

    Add BLE to BLE app

--- apps/ble/src/ble_service.cpp
+++ apps/ble/src/ble_service.cpp
@@ -1,8 +1,52 @@
-#include <zephyr/kernel.h>
-#include <zephyr/device.h>
-#include <zephyr/init.h>
+#include <stddef.h>
+#include <errno.h>
 
-int ble_service_init() {
+#include <zephyr/types.h>
+#include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
+
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/hci.h>
+#include <zephyr/bluetooth/conn.h>
+#include <zephyr/bluetooth/uuid.h>
+#include <zephyr/bluetooth/gatt.h>
+#include <zephyr/sys/byteorder.h>
+#include <zephyr/init.h>
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(BLE_Service);
+
+static const struct bt_data ad[] = {
+    BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
+};
+
+static const struct bt_data sd[] = {
+    BT_DATA(BT_DATA_NAME_COMPLETE, CONFIG_BT_DEVICE_NAME, sizeof(CONFIG_BT_DEVICE_NAME) - 1),
+};
+
+static void mtu_updated(struct bt_conn* conn, uint16_t tx, uint16_t rx)
+{
+    printk("Updated MTU: TX: %d RX: %d bytes\n", tx, rx);
+}
+static struct bt_gatt_cb gatt_callbacks = { .att_mtu_updated = mtu_updated };
+
+int ble_service_init()
+{
+    int rc = bt_enable(NULL);
+    if (rc) {
+        LOG_ERR("Bluetooth init failed (err %d)", rc);
+        return rc;
+    }
+    LOG_INF("Bluetooth initialized");
+
+    bt_gatt_cb_register(&gatt_callbacks);
+
+    rc = bt_le_adv_start(BT_LE_ADV_CONN_FAST_1, ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
+    if (rc) {
+        LOG_ERR("Advertising failed to start (err %d)", rc);
+        return rc;
+    }
+
+    LOG_INF("Advertising successfully started");
     return 0;
 }
 
