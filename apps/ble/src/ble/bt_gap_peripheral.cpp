668a6f6628d1274bc9150e900657504764446613
Milind Singh <milind345@gmail.com>
2025-12-26 00:42:03 -0800

    Add FTP service

--- apps/ble/src/ble/bt_gap_peripheral.cpp
+++ apps/ble/src/ble/bt_gap_peripheral.cpp
@@ -1,10 +1,12 @@
 #include <stddef.h>
 #include <errno.h>
+#include <array>
 
 #include <zephyr/types.h>
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
 
+#include <zephyr/bluetooth/att.h>
 #include <zephyr/bluetooth/bluetooth.h>
 #include <zephyr/bluetooth/hci.h>
 #include <zephyr/bluetooth/conn.h>
@@ -17,8 +19,19 @@ LOG_MODULE_REGISTER(BT_GAP_Peripheral);
 
 #include <ble/ble_init_prio.h>
 
+enum {
+    STATE_CONNECTED,
+    STATE_DISCONNECTED,
+    STATE_BITS,
+};
+
+static ATOMIC_DEFINE(state, STATE_BITS);
+
+std::array<uint8_t, 6u> mfg_data = { 0xAD, 0xDE, 'G', 'I', 'F', 'Y' };
+
 static const struct bt_data ad[] = {
     BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
+    BT_DATA(BT_DATA_MANUFACTURER_DATA, mfg_data.data(), mfg_data.size()),
 };
 
 static const struct bt_data sd[] = {
@@ -31,6 +44,64 @@ static void mtu_updated(struct bt_conn* conn, uint16_t tx, uint16_t rx)
     printk("Updated MTU: TX: %d RX: %d bytes\n", tx, rx);
 }
 static struct bt_gatt_cb gatt_callbacks = { .att_mtu_updated = mtu_updated };
+
+static void mtu_exchange_cb(struct bt_conn* conn, uint8_t err,
+                            struct bt_gatt_exchange_params* params)
+{
+    printk("%s: MTU exchange %s (%u)\n", __func__, err == 0U ? "successful" : "failed",
+           bt_gatt_get_mtu(conn));
+}
+
+static struct bt_gatt_exchange_params mtu_exchange_params = { .func = mtu_exchange_cb };
+
+static int mtu_exchange(struct bt_conn* conn)
+{
+    int err;
+
+    printk("%s: Current MTU = %u\n", __func__, bt_gatt_get_mtu(conn));
+
+    printk("%s: Exchange MTU...\n", __func__);
+    err = bt_gatt_exchange_mtu(conn, &mtu_exchange_params);
+    if (err) {
+        printk("%s: MTU exchange failed (err %d)", __func__, err);
+    }
+
+    return err;
+}
+
+static void connected(struct bt_conn* conn, uint8_t err)
+{
+    bt_conn_ref(conn);
+
+    (void)mtu_exchange(conn);
+
+    if (err) {
+        printk("Connection failed, err 0x%02x %s\n", err, bt_hci_err_to_str(err));
+    } else {
+        printk("Connected\n");
+        (void)atomic_set_bit(state, STATE_CONNECTED);
+    }
+}
+
+static void disconnected(struct bt_conn* conn, uint8_t reason)
+{
+    bt_conn_unref(conn);
+    printk("Disconnected, reason 0x%02x %s\n", reason, bt_hci_err_to_str(reason));
+
+    int rc = bt_le_adv_start(BT_LE_ADV_CONN_FAST_1, ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
+    if (rc) {
+        LOG_ERR("Advertising failed to start (err %d)", rc);
+        return;
+    }
+
+    LOG_INF("Advertising successfully started");
+    (void)atomic_set_bit(state, STATE_DISCONNECTED);
+}
+
+BT_CONN_CB_DEFINE(conn_callbacks) = {
+    .connected    = connected,
+    .disconnected = disconnected,
+};
 
 int bt_gap_init()
 {
--- apps/ble/src/ble/bt_gatt_devinfo.cpp
+++ apps/ble/src/ble/bt_gatt_devinfo.cpp
@@ -1,15 +1,36 @@
 #include <zephyr/kernel.h>
 #include <zephyr/init.h>
-#include <zephyr/logging/log.h>
 
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/hci.h>
+#include <zephyr/bluetooth/conn.h>
+#include <zephyr/bluetooth/uuid.h>
+#include <zephyr/bluetooth/gatt.h>
+#include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(BT_GATT_DEVINFO);
 
 #include <ble/ble_init_prio.h>
+
+#define BT_UUID_SVC_DEV_INFO    BT_UUID_DECLARE_32(0xDEADD00D)
+
+#define BT_UUID_ATTR_FW_VERSION BT_UUID_DECLARE_32(0x0001)
 
 int bt_gatt_devinfo_init()
 {
     LOG_INF("GATT Device Information Service Initialized");
     return 0;
 }
+
+ssize_t bt_gatt_devinfo_read_fw_version(struct bt_conn* conn, const struct bt_gatt_attr* attr,
+                                        void* buf, uint16_t len, uint16_t offset)
+{
+    const char* fw_version = "v1.0.0";
+    return bt_gatt_attr_read(conn, attr, buf, len, offset, fw_version, strlen(fw_version));
+}
+
+BT_GATT_SERVICE_DEFINE(devinfo_svc, BT_GATT_PRIMARY_SERVICE(BT_UUID_SVC_DEV_INFO),
+                       BT_GATT_CHARACTERISTIC(BT_UUID_ATTR_FW_VERSION, BT_GATT_CHRC_READ,
+                                              BT_GATT_PERM_READ, bt_gatt_devinfo_read_fw_version,
+                                              NULL, NULL), );
 
 SYS_INIT(bt_gatt_devinfo_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
